67056e3dc4010af90c77e1ab327c1db592b83707
hellosputnik <hallosputnik@gmail.com>
2018-07-08 21:21:07 -0700

    Add "Level 01" of Nebula

---
+++ Nebula/Level 01/level1.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+int main(int argc, char **argv, char **envp)
+{
+  gid_t gid;
+  uid_t uid;
+  gid = getegid();
+  uid = geteuid();
+
+  setresgid(gid, gid, gid);
+  setresuid(uid, uid, uid);
+
+  system("/usr/bin/env echo and now what?");
+}
+
