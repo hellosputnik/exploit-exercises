e618b8a2be60c3f4379c17793568f41673bc6133
hellosputnik <hallosputnik@gmail.com>
2018-09-09 13:32:30 -0700

    Add "Level 09" of Nebula

---
+++ Nebula/Level 09/level9.php
@@ -0,0 +1,27 @@
+<?php
+
+function spam($email)
+{
+  $email = preg_replace("/\./", " dot ", $email);
+  $email = preg_replace("/@/", " AT ", $email);
+
+  return $email;
+}
+
+function markup($filename, $use_me)
+{
+  $contents = file_get_contents($filename);
+
+  $contents = preg_replace("/(\[email (.*)\])/e", "spam(\"\\2\")", $contents);
+  $contents = preg_replace("/\[/", "<", $contents);
+  $contents = preg_replace("/\]/", ">", $contents);
+
+  return $contents;
+}
+
+$output = markup($argv[1], $argv[2]);
+
+print $output;
+
+?>
+
